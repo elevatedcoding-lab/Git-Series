8cb53f681f012f3a1a531c38b2210fcbf07ebedb
Robert Merrill <elevatedcoding@gmail.com>
2025-11-13 13:06:20 -0700

    Second commit with updated file

--- first_file.py
+++ first_file.py
@@ -1,7 +1,5 @@
-#This is a new file and our first commit.
-
-
+#This is a new file and our first commit
 
 #This is our code
-print("I love Git")
-print("maybe I should be slower and listen")
+
+print{"I lover Git")
